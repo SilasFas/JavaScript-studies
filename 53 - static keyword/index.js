fb9b3beae6fa293eb6110490bea0a1218b1ee700
Silas <silasfas@hotmail.com>
2023-05-24 17:51:47 -0300

    static keyword

--- 53 - static keyword/index.js
+++ 53 - static keyword/index.js
@@ -4,11 +4,48 @@
 
 class Car {
 
+    numberOfCars = 0
     constructor (model) {
         this.model = model;
+        this.numberOfCars += 1;
     }
+    
 }
 
 const car1 = new Car('Mustang')
 const car2 = new Car('Corvette')
 const car3 = new Car('BMW')
+
+console.log(car1.numberOfCars)
+console.log(car2.numberOfCars)
+console.log(car3.numberOfCars)
+
+
+
+console.log('')
+console.log('')
+
+
+
+class Carro {
+
+    static numeroDeCarros = 0
+
+    constructor (modelo) {
+        this.modelo = modelo;
+        Carro.numeroDeCarros += 1;
+    }
+
+    static começarCorrida (){
+        console.log('3...2...1...GO!')
+    }
+    
+}
+
+const carro1 = new Carro('Mustang')
+const carro2 = new Carro('Corvette')
+const carro3 = new Carro('BMW')
+
+console.log(Carro.numeroDeCarros)
+
+Carro.começarCorrida()
