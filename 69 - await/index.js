11b42dd876ea7a9b9c89826d9b4e2744316ad537
Silas <silasfas@hotmail.com>
2023-06-01 01:24:11 -0300

    await

--- 69 - await/index.js
+++ 69 - await/index.js
@@ -1,4 +1,6 @@
 // await = makes an async function wait for a Promise
+// You can onlu use the wait keyword within an async function
+// Eliminate .then methods
 
 async function loadFile() {
     let fileLoaded = true
@@ -10,6 +12,43 @@ async function loadFile() {
     }
 }
 
-loadFile()
-    .then((value) => console.log(value))
-    .catch((error) => console.log(error))
+async function startProcess() {
+
+    try {
+        let message = await loadFile()
+        console.log(message)
+    } 
+    catch (error) {
+        console.log(error)
+    }   
+}
+
+startProcess()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// await = makes an async function wait for a Promise
+
+// async function loadFile() {
+//     let fileLoaded = true
+
+//     if (fileLoaded) {
+//         return "File loaded"
+//     } else {
+//         throw "File NOT loaded"
+//     }
+// }
+
+// loadFile()
+//     .then((value) => console.log(value))
+//     .catch((error) => console.log(error))
