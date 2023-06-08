e8db5bb816302c9784af0a2101289e622d394c83
Silas <silasfas@hotmail.com>
2023-06-08 20:49:44 -0300

    element selectors

---
+++ 72 - Element selectors/index.js
@@ -0,0 +1,37 @@
+
+// getElementById
+// let element = document.getElementById('title')
+// element.style.backgroundColor = "lightgreen"
+
+//getElementsByName
+// let fruits = document.getElementsByName('fruits')
+// console.log(fruits[1])
+
+// fruits.forEach(fruit => {
+//     if (fruit.checked) {
+//         console.log(fruit.value)
+//     }
+// })
+
+//getElementsByTagName
+// let vegies = document.getElementsByTagName("li")
+// vegies[0].style.backgroundColor = "lightgreen"
+// vegies[2].style.backgroundColor = "lightblue"
+
+//getElementsByClassName
+// let desserts = document.getElementsByClassName("desserts")
+// desserts[0].style.backgroundColor = "lightblue"
+
+
+//querySelector
+
+// let element = document.querySelector("#title")
+// element.style.backgroundColor = "red"
+
+// let element = document.querySelector(".desserts")
+// element.style.backgroundColor = "red"
+
+// let elements = document.querySelectorAll("li")
+// elements.forEach(element => {
+//     element.style.backgroundColor = "lightgreen"
+// })
