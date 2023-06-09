43003319dc4ca56db3785c35eeca874ce2bfd127
Silas <silasfas@hotmail.com>
2023-06-09 00:05:30 -0300

    add-change CSS properties

---
+++ 75 - add-change CSS properties/index.js
@@ -0,0 +1,9 @@
+const tittle = document.getElementById('tittle')
+
+tittle.style.backgroundColor = 'rgb(255, 0, 255)'
+tittle.style.color = '#fff'
+tittle.style.fontFamily ='consolas'
+tittle.style.textAlign = 'center'
+tittle.style.border = '2px solid black'
+tittle.style.display = 'block'
+
