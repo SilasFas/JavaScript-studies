c551870e1105b96c932e8b9aed7c7d5a887cae14
Silas <silasfas@hotmail.com>
2023-06-18 16:21:45 -0300

    A propriedade constructor

---
+++ JS OOP/15 - A propriedade constructor/index.js
@@ -0,0 +1,19 @@
+function criarArvore(especie, temFruto) {
+    return {
+        especie: especie,
+        temFruto: temFruto
+    }
+}
+
+let laranjeira = criarArvore('laranjeira', true)
+console.log(laranjeira)
+console.log(laranjeira.constructor)
+
+function Heroi(nome, classe) {
+    this.nome = nome
+    this.classe = classe
+}
+
+let jaspion = new Heroi('Jaspion', 'robô')
+console.log(jaspion)
+console.log(jaspion.constructor)
