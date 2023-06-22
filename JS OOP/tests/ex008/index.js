d70ac6b198ea51aa21c800161b4c3818bff514ce
Silas <silasfas@hotmail.com>
2023-06-22 19:54:13 -0300

    ex008

---
+++ JS OOP/tests/ex008/index.js
@@ -0,0 +1,13 @@
+truck = {
+    rodas: 4,
+    cor: 'cinza',
+    capacidade: '300',
+    portas: 2
+}
+
+let {rodas, cor, capacidade, portas} = truck
+
+console.log(rodas)
+console.log(cor)
+console.log(capacidade)
+console.log(portas)
