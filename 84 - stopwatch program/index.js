f71c70e188383534af83635732010c8582e5c56f
Silas <silasfas@hotmail.com>
2023-06-24 10:34:22 -0300

    finish

--- 84 - stopwatch program/index.js
+++ 84 - stopwatch program/index.js
@@ -13,26 +13,36 @@ let hours = 0
 let mins = 0
 let secs = 0
 
-
-
-
-
-
-
 startBtn.addEventListener('click', () => {
     if (paused) {
         paused = false
         startTime = Date.now() - elapsedtime
         // Date.now() gets the time in miliseconds
         intervalId = setInterval(updateTime, 75) // 1000ms
-
-
-
-
     }
 })
-pauseBtn.addEventListener('click', () => { })
-resetBtn.addEventListener('click', () => { })
+
+pauseBtn.addEventListener('click', () => {
+    if (!paused) {
+        paused = true
+        elapsedtime = Date.now() - startTime
+        clearInterval(intervalId)
+    }
+})
+
+resetBtn.addEventListener('click', () => {
+    paused = true
+    clearInterval(intervalId)
+
+    startTime = 0
+    elapsedtime = 0
+    currentTime = 0
+    hours = 0
+    mins = 0
+    secs = 0
+    timeDisplay.textContent = '00:00:00'
+
+})
 
 function updateTime() {
     elapsedtime = Date.now() - startTime
@@ -45,11 +55,10 @@ function updateTime() {
     mins = pad(mins)
     hours = pad(hours)
 
-    timeDisplay.textContent = `${hours}:${mins}:${secs}`
-
     function pad(unit) {
         return (("0") + unit).length > 2 ? unit : "0" + unit
     }
+    timeDisplay.textContent = `${hours}:${mins}:${secs}`
 }
 
 
