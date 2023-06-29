ebc414a251b0bbbef7fdff7d3826407258631833
Silas <silasfas@hotmail.com>
2023-06-29 15:20:43 -0300

    Number

---
+++ JS OOP/35 - Number/index.js
@@ -0,0 +1,13 @@
+let a = 2
+let b = new Number(3)
+
+console.log(a)
+console.log(b)
+console.log(b.valueOf())
+
+let c = 12.555 
+console.log(Number.parseInt(c))
+console.log(parseInt(c))
+
+console.log(Number.parseFloat(c))
+console.log(parseFloat(c))
