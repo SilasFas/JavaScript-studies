16b00a4803b0633823ec39f6c0c1e3ac7dc54113
Silas <silasfas@hotmail.com>
2023-06-29 14:57:54 -0300

    function

--- JS OOP/32 - function/index.js
+++ JS OOP/32 - function/index.js
@@ -2,10 +2,17 @@ function teste(a) {
     return a
 }
 console.log(teste('opa'))
+console.log(teste.length)
+console.log(teste.toString())
+
+console.log(teste instanceof Function)
+console.log(teste instanceof Object)
 
 
+console.log('')
 
 
+// não é usado
 teste2 = new Function(
     'a',
     'return arguments'
