c8c7ca0382b17d0dd37258613ab72af17c2b5de4
Silas <silasfas@hotmail.com>
2023-06-29 14:21:08 -0300

    object

---
+++ JS OOP/29 - object/index.js
@@ -0,0 +1,16 @@
+let o1 = {}
+
+let o2 = new Object()
+
+console.log(o1)
+console.log(o2)
+
+
+o1.nome = 'Silas'
+o2.nome = 'Pedro'
+
+console.log(o1.valueOf())
+console.log(o2.valueOf())
+
+console.log(o1.toString())
+console.log(o2.toString())
