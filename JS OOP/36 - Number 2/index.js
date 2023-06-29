a979dcc7079358ebf08d29d6aa21d020a91ee4d1
Silas <silasfas@hotmail.com>
2023-06-29 15:28:11 -0300

    Number

---
+++ JS OOP/36 - Number 2/index.js
@@ -0,0 +1,8 @@
+console.log(Number)
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+console.log(Number.MAX_SAFE_INTEGER)
+console.log(Number.MIN_SAFE_INTEGER)
+console.log(Number.NaN)
+console.log(Number.NEGATIVE_INFINITY)
+console.log(Number.POSITIVE_INFINITY)
