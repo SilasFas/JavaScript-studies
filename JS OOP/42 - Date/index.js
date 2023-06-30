a7bec0f4d61bd0e2dfecdd33a8602a6c9a8573f4
Silas <silasfas@hotmail.com>
2023-06-30 14:22:07 -0300

    date

--- JS OOP/42 - Date/index.js
+++ JS OOP/42 - Date/index.js
@@ -1,4 +1,11 @@
 console.log(new Date())
+console.log(Date.now()) // data atual, porém em milisegundos (usado para calcular periodo entre datas)
 console.log(new Date(2021, 10, 24))
 
+let data = new Date()
+console.log(data)
+console.log(new Date(data.setMonth(0)))
 
+console.log(new Date(Date.now()))
+
+console.log(Date.parse(new Date(data.setMonth(0))))
