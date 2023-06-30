b1b5349ba8b9d0c3c21ad738e8a67f0e121449aa
Silas <silasfas@hotmail.com>
2023-06-30 13:35:52 -0300

    string

---
+++ JS OOP/39 - String 2/index.js
@@ -0,0 +1,13 @@
+let str = 'Matheus'
+console.log(str.toLocaleUpperCase())
+
+let upper = str.toLocaleUpperCase()
+
+console.log(upper)
+console.log(upper.toLocaleLowerCase())
+
+console.log(str.charAt(5))
+console.log(str[5])
+console.log(str.indexOf('eus'))
+
+
