2bafd1394c33ec60701a4385ede711b1f6a8ea1d
Silas <silasfas@hotmail.com>
2023-06-30 13:52:15 -0300

    math

--- JS OOP/41 - Math/index.js
+++ JS OOP/41 - Math/index.js
@@ -4,3 +4,26 @@ console.log(Math.LN2)
 console.log(Math.LN10)
 
 console.log(Math.floor(Math.random() * 11))
+
+console.log(Math.floor(12.384))
+console.log(Math.ceil(12.384))
+
+console.log(Math.pow(2,2))
+console.log(Math.pow(12,4))
+
+console.log(Math.sqrt(16))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
