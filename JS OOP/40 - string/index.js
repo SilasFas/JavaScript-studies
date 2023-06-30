72985f8a1894722dcf600866f004306dddadf9af
Silas <silasfas@hotmail.com>
2023-06-30 13:43:47 -0300

    string

---
+++ JS OOP/40 - string/index.js
@@ -0,0 +1,12 @@
+let frase = 'O rato roeu a roupa do rei de Roma'
+
+console.log(frase.slice(2, 6))
+
+console.log(frase.split(' '))
+
+console.log('teste'.split(''))
+
+let teste = frase.split(' ')
+teste.forEach((test) => {
+    console.log(test)
+})
